6cbe4b2e2711ae0106d8cdcfca87672d82bbde5e
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2021-10-14 18:20:21 +0200

    handler/project: print current projects in DB
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- app/handler/projects.go
+++ app/handler/projects.go
@@ -10,8 +10,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-const projects = `<ul>{{ range . }}
-<li>{{ .Name }}</li>
+const projects = `<ul>{{ range $proj := .}}
+{{ range .Versions }}
+<li>{{ $proj.Name }}: <a href="/{{ $proj.Name | lower }}@{{ . | lower }}">{{ . }}</a></li>
+{{ end }}
 {{ end }}</ul>`
 
 var (
@@ -20,11 +22,31 @@ var (
 	}
 )
 
+type Projects struct {
+	Name     string
+	Versions []string
+}
+
 func GetAllProjects(db db.DB, w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	p := db.GetProjects()
 	tmpl, _ := template.New("projects").Funcs(funcMap).Parse(projects)
-	tmpl.Execute(w, p)
+
+	rendered := make([]Projects, len(p))
+
+	// TODO: this is super slow, do it into the DB.
+	for i, project := range p {
+		rendered[i].Name = project.Name
+
+		versions := db.GetVersionsForProjects(project.ID)
+
+		for _, version := range versions {
+			rendered[i].Versions = append(rendered[i].Versions, version.Version)
+		}
+
+	}
+
+	tmpl.Execute(w, rendered)
 }
 
 func GetProject(db db.DB, w http.ResponseWriter, r *http.Request) {
