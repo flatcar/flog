2af6603b22a5851d96762ca919bc284674624da6
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2021-10-14 18:51:27 +0200

    handle/project: display changelog for one project
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- app/handler/projects.go
+++ app/handler/projects.go
@@ -3,28 +3,53 @@ package handler
 import (
 	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
 
+	"github.com/flatcar-linux/flog/pkg/connector"
 	"github.com/flatcar-linux/flog/pkg/db"
 
 	"github.com/gorilla/mux"
 )
 
-const projects = `<ul>{{ range $proj := .}}
+const (
+	projects = `<ul>{{ range $proj := .}}
 {{ range .Versions }}
-<li>{{ $proj.Name }}: <a href="/{{ $proj.Name | lower }}@{{ . | lower }}">{{ . }}</a></li>
+<li>{{ $proj.Name }}: <a href="/projects/{{ $proj.Name | lower | escape }}@{{ . | lower }}">{{ . }}</a></li>
 {{ end }}
 {{ end }}</ul>`
+	change = `<h1>{{ .Name }} - {{ .Version }}</h1><hr/>
+{{ .Content }}
+<br />
+<a href="{{ .Source }}">source</a>`
+)
 
 var (
 	funcMap = template.FuncMap{
-		"lower": strings.ToLower,
+		"lower":  strings.ToLower,
+		"escape": url.QueryEscape,
+	}
+
+	// connectors is the list of available connectors
+	connectors = map[string]connector.Connector{
+		"github": &connector.GH{},
 	}
 )
 
+// helpers for templates
+
+// Projects
 type Projects struct {
 	Name     string
 	Versions []string
+}
+
+// Changelog
+type Changelog struct {
+	Name    string
+	Version string
+	Content template.HTML
+	Source  string
 }
 
 func GetAllProjects(db db.DB, w http.ResponseWriter, r *http.Request) {
@@ -52,6 +77,38 @@ func GetAllProjects(db db.DB, w http.ResponseWriter, r *http.Request) {
 func GetProject(db db.DB, w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
-	id := vars["id"]
-	respondJSON(w, http.StatusOK, id)
+	id, ok := vars["id"]
+	if !ok {
+		http.Error(w, "id is not here", 406)
+	}
+
+	// TODO: handle me
+	id, _ = url.QueryUnescape(id)
+
+	project := strings.Split(id, "@")
+	if len(project) != 2 {
+		// TODO: handle me
+		http.Error(w, "id should be project@version", 406)
+	}
+
+	connectorName := db.GetConnector(project[0])
+	conn, ok := connectors[connectorName]
+	if !ok {
+		// TODO: handle me
+		http.Error(w, "connector is not yet implemented", 406)
+	}
+
+	// TODO: handle me
+	content, URL, _ := conn.FetchChangelog(project[0], project[1])
+
+	changelog := &Changelog{
+		Name:    project[0],
+		Version: project[1],
+		Content: template.HTML(content),
+		Source:  URL,
+	}
+
+	// TODO: handle me
+	tmpl, _ := template.New("change").Funcs(funcMap).Parse(change)
+	tmpl.Execute(w, changelog)
 }
